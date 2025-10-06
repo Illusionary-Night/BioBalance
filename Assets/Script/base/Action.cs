2cda7da9568369369965557be8a0b11fd3a8d2f6
yuyuching <87879794+yuyuching931216@users.noreply.github.com>
2025-10-07 00:30:48 +0800

    Change the Unity version to 6000.2.6f2, add the abstract class ActionBase and static class ActionSystem, add the new enum ActionType

--- Assets/Script/base/Action.cs
+++ Assets/Script/base/Action.cs
@@ -1,10 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public abstract class Action
+using System.Linq;
+using System.Reflection;
+
+public static class ActionSystem
 {
-    public abstract bool isConditionMet();
-    public abstract float getWeight();
-    public abstract bool isSuccess();
-    public abstract void execute(Creature creature);
+    private static readonly Dictionary<ActionType, ActionBase> actions = new();
+
+    static ActionSystem()
+    {
+        ActionSystem.Register();
+    }
+
+    public static void Register()
+        // 掃描所有繼承 ActionBase 的型別
+        var actionTypes = typeof(ActionBase).Assembly.GetTypes()
+            .Where(t => !t.IsAbstract && t.IsSubclassOf(typeof(ActionBase)));
+
+        foreach (var type in actionTypes)
+        {
+            // 強制建立該類別的 Instance 靜態欄位
+            var instanceField = type.GetField("Instance", BindingFlags.Public | BindingFlags.Static);
+            if (instanceField?.GetValue(null) is ActionBase action)
+                actions.Add(action.Type, action);
+        }
+{
+    }
+
+    public static bool isConditionMet(Creature creature, ActionType actiontype)
+    {
+        return actions.TryGetValue(actiontype, out var f) && f.IsConditionMet(creature);
+    }
+
+    public static float getWeight(Creature creature, ActionType actiontype)
+    {
+        return actions.TryGetValue(actiontype, out var f) ? f.GetWeight(creature) : 0f;
+    }
+
+    public static bool isSuccess(Creature creature, ActionType actiontype)
+    {
+        return actions.TryGetValue(actiontype, out var f) && f.IsSuccess(creature);
+    }
+
+    public static void execute(Creature creature, ActionType actiontype)
+    {
+        if (actions.TryGetValue(actiontype, out var f))
+            f.Execute(creature);
+    }
 }
+
+public abstract class ActionBase
+{
+    public abstract ActionType Type { get; }
+    public abstract int Cooldown { get; }
+
+    public abstract bool IsConditionMet(Creature creature);
+    public abstract float GetWeight(Creature creature);
+    public abstract bool IsSuccess(Creature creature);
+    public abstract void Execute(Creature creature);
+}
+
+
+
